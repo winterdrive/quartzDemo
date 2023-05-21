b765f3d31abdb1798c00877d6503a3fd078f8ec2
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-05-22 01:27:53 +0800

    Initial commit

---
+++ src/main/java/com/winston/quartzdemo/QuartzConfig.java
@@ -0,0 +1,26 @@
+package com.winston.quartzdemo;
+
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail testQuartz1() {
+        return JobBuilder.newJob(TestJob.class).withIdentity("testTask1").storeDurably().build();
+    }
+
+    @Bean
+    public Trigger testQuartzTrigger1() {
+        //5秒执行一次
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(5)
+                .repeatForever();
+        return TriggerBuilder.newTrigger().forJob(testQuartz1())
+                .withIdentity("testTask1")
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+}
